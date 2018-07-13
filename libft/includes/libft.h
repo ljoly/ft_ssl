2b81804cf5b4b72a0b5a148d0aa3e5dc6e222cb4
Louis-Marie Joly <ljoly@student.42.fr>
2018-07-13 23:44:39 +0200

    atohex

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/24 16:14:13 by ljoly             #+#    #+#             */
-/*   Updated: 2018/07/10 17:13:42 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/07/12 16:51:52 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,7 @@ int				ft_strcmp(const char *s1, const char *s2);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_atoi(const char *str);
 ssize_t			ft_atoss(const char *str);
+char			*ft_atohex(const char *src, int upper);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
 int				ft_isalnum(int c);
--- src/md5.c
+++ src/md5.c
@@ -6,59 +6,16 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/04 19:13:19 by ljoly             #+#    #+#             */
-/*   Updated: 2018/07/10 17:54:02 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/07/12 19:15:17 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl.h"
 
-static void fill_dst(char *dst, char *tab, size_t nb)
-{
-    if (nb < 16)
-        *dst = tab[nb];
-    if (nb >= 16)
-    {
-        fill_dst(dst, tab, nb / 16);
-        *dst = tab[nb % 16];
-        dst++;
-    }
-}
-
-static void     ft_atohex(const char *src, char *dst, int upper)
-{
-    char        *tab;
-    char        *tmp;
-    size_t      len;
-    size_t      i;
-
-    tab = (upper == 1) ? HEX_UP : HEX;
-    printf("src = %zu\ndst = %zu\n", ft_strlen(src), ft_strlen(dst));
-    // if ((len = ft_strlen(src)) && ft_strlen(dst) == 2 * len)
-    // {
-        len = ft_strlen(src);
-        i = 0;
-        tmp = dst;
-        while (i < len)
-        {
-            fill_dst(tmp, tab, ft_atoi(&src[i]));
-    printf("%s\n", tmp);
-            
-            i++;
-        }
-        dst[i] = '\0';
-    printf("%s\n", tmp);
-    
-    // }
-}
-
 static void add_padding(t_env *e)
 {
-    size_t size;
-
-    size = 2 * ft_strlen(e->input) + 1;
-    if (!(e->padded_msg = (char *)ft_memalloc(sizeof(char) * size)))
-        ft_err(MALLOC);
-    ft_atohex(e->input, e->padded_msg, FALSE);
+    // add 64-bit size
+    ft_printf("msg = %s\n", e->padded_msg);
 }
 
 static char *get_padding(int size)
@@ -91,10 +48,16 @@ void ft_md5(char *s)
 {
     t_env e;
 
+
     e.input = s;
-    e.bin_padding = NULL;
-    e.bitsize = ft_strlen(s) * 8;
-    e.bin_padding = get_padding(e.bitsize);
-    // printf("padding = %s\n%zu\n", e.bin_padding, ft_strlen(e.bin_padding));
+    // input to hex    
+    if (!(e.padded_msg = ft_atohex(e.input, FALSE)))
+        ft_err(MALLOC);
+    e.input_bitsize = ft_strlen(s) * 8;
+    // get binary padding
+    e.padding = get_padding(e.input_bitsize);
+
+    ft_printf("padding = %s\n", e.padding);
+    ft_printf("padding size = %zu\n", ft_strlen(e.padding));
     add_padding(&e);
 }
