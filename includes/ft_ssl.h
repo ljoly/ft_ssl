916228bfc4694e1695bf5a28791339bca3320fd1
Louis-Marie Joly <ljoly@student.42.fr>
2018-09-18 19:21:24 +0200

    padding + size ok

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -6,7 +6,7 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/04 18:03:23 by ljoly             #+#    #+#             */
-/*   Updated: 2018/09/12 16:05:28 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/09/18 14:26:41 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,11 @@ uint32_t        g_s[64];
 typedef struct  s_env
 {
     char        *input;
+    size_t      input_len;
     uint32_t    padding_bitsize;
     uint32_t    input_bitsize;
-    int         blocks;
+    size_t      blocks;
+    uint32_t    *meta_block;
 }				t_env;
 
 void		    ft_md5(char *s);
--- src/md5.c
+++ src/md5.c
@@ -6,11 +6,73 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/04 19:13:19 by ljoly             #+#    #+#             */
-/*   Updated: 2018/09/12 19:19:16 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/09/18 19:20:45 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl.h"
+
+// static uint32_t reverse_bits(uint32_t value)
+// {
+//     return (value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 |
+//         (value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24;
+// }
+
+static void     add_size(t_env *e)
+{
+    size_t      size;
+    size_t      index;
+
+    size = e->input_bitsize - 64;
+    index = e->blocks * 16 - 2;
+    e->meta_block[index + 1] = (size >> 32) & 0xffffffff;
+    e->meta_block[index] = size & 0xffffffff;
+}
+
+static void     add_padding(t_env *e, size_t len_buff, size_t index_meta)
+{
+    uint32_t    pad;
+
+    pad = 0x00000080;
+    if (len_buff == 1)
+        pad = 0x00008000;
+    else if (len_buff == 2)
+        pad = 0x00800000;
+    else if (len_buff == 3)
+        pad = 0x80000000;
+    else if (len_buff == 4)
+    {
+        index_meta++;
+    }
+    e->meta_block[index_meta] = e->meta_block[index_meta] | pad;
+}
+
+static void     build_meta(t_env *e)
+{
+    size_t      i;
+    size_t      j;
+    size_t      k;
+    char        buff[5];
+
+    e->meta_block = (uint32_t*)ft_memalloc(sizeof(uint32_t) * e->blocks * 16);
+    i = 0;
+    k = 0;
+    while (i < e->input_len)
+    {
+        j = 0;
+        while (i < e->input_len && j < 4)
+        {
+            buff[j] = e->input[i];
+            j++;
+            i++;
+        }
+        buff[j] = '\0';
+        ft_memcpy(&e->meta_block[k], buff, ft_strlen(buff));
+        k++;
+    }
+    add_padding(e, ft_strlen(buff), k - 1);
+    add_size(e);
+}
 
 static void     get_format(t_env *e)
 {
@@ -28,15 +90,28 @@ static void     get_format(t_env *e)
     ft_printf("padding size = %d\n", e->padding_bitsize);
 }
 
-void ft_md5(char *s)
+void static     print(t_env e)
+{
+    size_t         i;
+
+    i = 0;
+    while (i < e.blocks * 16)
+    {
+        printf("%u\n", e.meta_block[i]);
+        i++;
+    }
+}
+
+void ft_md5(char *input)
 {
     t_env       e;
-    uint32_t    message[16];
 
-    e.input = s;
-    e.input_bitsize = ft_strlen(s) * 8;
-    ft_printf("input len = %zu\n", ft_strlen(s));
+    e.input = input;
+    e.input_len = ft_strlen(input);
+    e.input_bitsize = e.input_len * 8;
+    ft_printf("input len = %zu\n", ft_strlen(input));
     ft_printf("input bitsize = %d\n", e.input_bitsize);
     get_format(&e);
-    message[0] = s[0];
+    build_meta(&e);
+    print(e);
 }
