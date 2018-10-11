190371bc456e6285507c136ef9e023ce744e4cb4
Louis-marie JOLY <ljoly@e2r10p4.42.fr>
2018-10-11 12:11:29 +0200

    norme headers

--- includes/error.h
+++ includes/error.h
@@ -6,29 +6,29 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 18:05:45 by ljoly             #+#    #+#             */
-/*   Updated: 2018/10/10 18:16:45 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/10/11 12:07:07 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
 
-typedef enum		e_sys
+typedef enum	e_sys
 {
-    READ,
-    MALLOC,
-}					t_sys;
+	READ,
+	MALLOC,
+}				t_sys;
 
-typedef enum        e_usage
+typedef enum	e_usage
 {
-    NOT_A_FILE,
-    NO_STRING,
-    NOT_A_HASH,
-    ILLEGAL_OPTION,
+	NOT_A_FILE,
+	NO_STRING,
+	NOT_A_HASH,
+	ILLEGAL_OPTION,
 	IS_DIRECTORY
-}                   t_usage;
+}				t_usage;
 
-void                err_sys(t_sys err_code);
-void                err_usage(t_usage err_code, t_flags *flags);
+void			err_sys(t_sys err_code);
+void			err_usage(t_usage err_code, t_flags *flags);
 
 #endif
--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/26 20:43:00 by ljoly             #+#    #+#             */
-/*   Updated: 2018/10/10 19:11:36 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/10/11 12:11:13 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,51 @@
 # include "libft.h"
 # include "sys/stat.h"
 
-typedef enum    e_run
+typedef enum	e_run
 {
-    START,
-    USE,
-    END,
-}               t_run;
+	START,
+	USE,
+	END,
+}				t_run;
 
-typedef enum    e_name
+typedef enum	e_name
 {
-    MD5,
-    SHA224,
-    SHA256,
-    SHA512,
-}               t_name;
+	MD5,
+	SHA224,
+	SHA256,
+	SHA512,
+}				t_name;
 
 /*
 ** Handle flags and arguments. Input will be copied into a char*
 */
 
-typedef struct  s_flags
+typedef struct	s_flags
 {
-    t_name      algo;
-    t_bool      sha256;
-    t_bool      p;
-    t_bool      q;
-    t_bool      r;
-    t_bool      s;
-    t_bool      file_open;
-    t_bool      stdin_open;
-    t_bool      error_seen;
-    t_bool      error;
-    char        *input; // copy the string/file/arg here
+	t_name		algo;
+	t_bool		sha256;
+	t_bool		p;
+	t_bool		q;
+	t_bool		r;
+	t_bool		s;
+	t_bool		file_open;
+	t_bool		stdin_open;
+	t_bool		error_seen;
+	t_bool		error;
+	char		*input;
 	size_t		size;
-    int         hashes;
+	int			hashes;
 }				t_flags;
 
-void	        handle_args(int action, char *arg, t_flags *flags);
-void            read_fd(t_flags *flags, int fd);
-void            *state_a(char *arg, t_flags *flags);
-void            *state_b(char *arg, t_flags *flags);
-void            *state_c(char *arg, t_flags *flags);
+void			handle_args(int action, char *arg, t_flags *flags);
+void			read_fd(t_flags *flags, int fd);
+void			*state_a(char *arg, t_flags *flags);
+void			*state_b(char *arg, t_flags *flags);
+void			*state_c(char *arg, t_flags *flags);
 
-t_bool          lex_hashname(char *arg, t_flags *flags);
-t_bool          lex_flags_pqr(char *arg, t_flags *flags);
-t_bool          lex_flag_s(char *arg);
-t_bool          lex_file(char *arg, t_flags *flags);
+t_bool			lex_hashname(char *arg, t_flags *flags);
+t_bool			lex_flags_pqr(char *arg, t_flags *flags);
+t_bool			lex_flag_s(char *arg);
+t_bool			lex_file(char *arg, t_flags *flags);
 
 #endif
